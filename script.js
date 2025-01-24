3997fd75bf0afc1bbb54b5476de933467cc29ecf
AmandeepSingh024 <148731892+AmandeepSingh024@users.noreply.github.com>
2025-01-23 18:02:55 -0800

    new commit-Aman
    Sample test

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    const message = document.getElementById("message");
+    message.textContent = "Hello! You clicked the button!";
+    message.style.color = "#007bff";
+}
